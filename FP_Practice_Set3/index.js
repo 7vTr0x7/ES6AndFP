86edc3d4e8b6f6e76b9aa19c3f500d68ba5c763a
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 12:53:41 +0530

    FP_Practice_Set3 - getTitlesWithMoreThan700Pages

--- FP_Practice_Set3/index.js
+++ FP_Practice_Set3/index.js
@@ -144,3 +144,24 @@ const getCarModel = (cars) => {
 const carModels = getCarModel(newCarsArray);
 console.log(carModels);
 // Output: ["Camry", "Mustang"]
+
+console.log("-------------------------------------");
+
+const booksArray = [
+  { title: "The Lord of the Rings", author: "J.R.R. Tolkien", pageCount: 1178 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", pageCount: 281 },
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    pageCount: 193,
+  },
+  { title: "The Name of the Wind", author: "Patrick Rothfuss", pageCount: 662 },
+];
+
+const getTitlesWithMoreThan700Pages = (books) => {
+  const filteredBooks = books.filter((book) => book.pageCount > 700);
+  return filteredBooks.map((book) => book.title);
+};
+const booksWithMoreThan700Pages = getTitlesWithMoreThan700Pages(booksArray);
+console.log(booksWithMoreThan700Pages);
+// Output: ["The Lord of the Rings"]
