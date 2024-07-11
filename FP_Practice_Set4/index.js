492704976c43a260d155ce77f60f17e1d9d0d84f
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 14:53:52 +0530

    FP_Practice_Set4 - sumSquares

--- FP_Practice_Set4/index.js
+++ FP_Practice_Set4/index.js
@@ -7,3 +7,12 @@ console.log(totalLength(strings));
 // Output: 30
 
 console.log("------------------------------");
+
+const numbers = [1, 2, 3, 4, 5];
+// Your code here
+const sumSquares = (numbers) =>
+  numbers.reduce((acc, curr) => acc + curr * curr, 0);
+console.log(sumSquares(numbers));
+// Output: 55
+
+console.log("------------------------------");
