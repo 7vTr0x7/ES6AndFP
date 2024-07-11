b99ebb71e2140ccab470676e8cb7193685be13ea
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 11:54:53 +0530

    FP_Practice_Set2 - getProducts

--- FP_Practice_Set2/index.js
+++ FP_Practice_Set2/index.js
@@ -68,3 +68,17 @@ console.log(studentNames);
 // Output: ["Anjali", "Arpit", "Ankit"]
 
 console.log("------------------------------------");
+
+const productData = [
+  { prodName: "Dairy Milk", price: 10 },
+  { prodName: "Dairy Milk Silk", price: 70 },
+  { prodName: "Five Star", price: 20 },
+  { prodName: "Mars", price: 50 },
+];
+// Your code here
+const getProducts = (products) => products.filter((prod) => prod.price > 40);
+
+console.log(getProducts(productData, 40));
+// Output: [{prodName: 'Dairy Milk Silk', price: 70}, {prodName: 'Mars', price: 50}]
+
+console.log("------------------------------------");
