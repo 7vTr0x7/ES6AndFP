22edc3f6adbd75b85ffc4b1193b09ff1c93b6ae0
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 15:20:54 +0530

    FP_Practice_Set4 - countStrings

--- FP_Practice_Set4/index.js
+++ FP_Practice_Set4/index.js
@@ -97,3 +97,23 @@ const findMostExpensiveProduct = (products) =>
 
 console.log(findMostExpensiveProduct(products));
 // { name: "Slipper", price: 40, quantity: 3 }
+
+const fruits = [
+  "apple",
+  "banana",
+  "banana",
+  "cherry",
+  "apple",
+  "apple",
+  "banana",
+];
+// Your code here
+
+const countStrings = (fruits) =>
+  fruits.reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {});
+
+console.log(countStrings(fruits));
+// Output: { 'apple': 3, 'banana': 3, 'cherry': 1 }
