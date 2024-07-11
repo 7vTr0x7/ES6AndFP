49ef78431dc9fe593c9e77245e3a7d4834ce3153
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 12:22:23 +0530

    FP_Practice_Set3 - filterStudentsByGrade

--- FP_Practice_Set3/index.js
+++ FP_Practice_Set3/index.js
@@ -57,3 +57,18 @@ console.log(electronicProducts);
 // Output: [{ name: "iPad", price: 799, category: "electronics" }, { name: "Smartwatch", price: 199, category: "electronics" }]
 
 console.log("-------------------------------------");
+
+const students = [
+  { name: "Alice", age: 16, grade: 90 },
+  { name: "Bob", age: 17, grade: 80 },
+  { name: "Charlie", age: 15, grade: 95 },
+  { name: "David", age: 16, grade: 85 },
+];
+const filterStudentsByGrade = (students, grade) =>
+  students.filter((student) => student.grade > grade);
+
+const highGradeStudents = filterStudentsByGrade(students, 85);
+console.log(highGradeStudents);
+// Output: [{ name: "Alice", age: 16, grade: 90 }, { name: "Charlie", age: 15, grade: 95 }]
+
+console.log("-------------------------------------");
