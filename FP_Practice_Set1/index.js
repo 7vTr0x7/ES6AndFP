7db95a56c7b704b20fc2343b371234a6e2ab90f5
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 11:26:43 +0530

    FP_Practice_Set1 - filterBySalary

--- FP_Practice_Set1/index.js
+++ FP_Practice_Set1/index.js
@@ -129,3 +129,17 @@ const filteredBooks = filterByGenre(books, "Science Fiction");
 console.log(filteredBooks);
 // Output: [{title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction"}, {title: "1984", author: "George Orwell", genre: "Science Fiction"}]
 console.log("----------------------------");
+
+const filterBySalary = (employees, salary, age) =>
+  employees.filter((emp) => emp.salary > salary && emp.age > age);
+
+const employees = [
+  { name: "Abhay", age: 25, salary: 20000 },
+  { name: "Joice", age: 30, salary: 35000 },
+  { name: "Reena", age: 35, salary: 15000 },
+  { name: "Jeena", age: 40, salary: 50000 },
+];
+
+const filteredEmployees = filterBySalary(employees, 22000, 25);
+console.log(filteredEmployees);
+// Output: [{name: "Joice", age: 30, salary: 35000}, {name: "Jeena", age: 40, salary: 50000}]
