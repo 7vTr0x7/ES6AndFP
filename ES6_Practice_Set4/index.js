373886d53e02702b0edb06f6ad58b97ab511f713
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 10:28:50 +0530

    ES6_Practice_Set4 - calculateAverage

--- ES6_Practice_Set4/index.js
+++ ES6_Practice_Set4/index.js
@@ -11,3 +11,15 @@ const getMaxElement = (arr) => {
 };
 
 console.log(getMaxElement(array)); // 78
+
+console.log("-------------------------------");
+
+const calculateAverage = (arr) => {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  return total / arr.length;
+};
+console.log(calculateAverage([1, 2, 3, 4, 5])); // 3
